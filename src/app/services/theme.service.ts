43c5147bc35f09fbc4a5395048a88c1e438d1a29
vancampk <vancampk89@gmail.com>
2025-07-27 19:23:56 -0400

    Adds new theme options
    Introduces new theme options, expanding the color palette choices for users.
    
    The new themes are: Mallard Duck, Loon, Painted Bunting, and Lazuli Bunting.

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,7 +1,20 @@
 import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 import { SettingsService } from './settings.service';
 
-export const THEMES = ['default-theme', 'blue-jay-theme', 'cardinal-theme', 'goldfinch-theme', 'downy-woodpecker-theme', 'black-capped-chickadee-theme', 'hummingbird-theme', 'robin-theme'];
+export const THEMES = [
+  'default-theme',
+  'blue-jay-theme',
+  'cardinal-theme',
+  'goldfinch-theme',
+  'downy-woodpecker-theme',
+  'black-capped-chickadee-theme',
+  'hummingbird-theme',
+  'robin-theme',
+  'mallard-duck-theme',
+  'loon-theme',
+  'painted-bunting-theme',
+  'lazuli-bunting-theme'
+];
 
 @Injectable({
   providedIn: 'root'
